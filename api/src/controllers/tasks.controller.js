34b4b6875cf06d19e50904edce05f6e890d66482
Jean <jean.ardaillou1@gmail.com>
2026-05-19 19:09:22 +0200

    réécriture des controllers avec Prisma et async/await

--- api/src/controllers/tasks.controller.js
+++ api/src/controllers/tasks.controller.js
@@ -1,62 +1,45 @@
 import { prisma } from '../db.js';
 
-// GET all
-// jutilise le truxc de prisma prisma.task.FinUnique()
-
-export const getTask = (req, res, next) => {
-
-  const task = await prisma.task.FindUnique();
-
-  if (!task) {
-    return res.status(404).json({ message: "Task not found" }).end();
-  }
-
-  res.status(200).json(task).end();
-};
-
-export const getTasks = (req, res, next) => {
+export const getTasks = async (req, res, next) => {
+  const tasks = await prisma.task.findMany();
   res.status(200).json(tasks).end();
 };
 
-export const createTask = (req, res, next) => {
-  const { title, description } = req.body;
-  const newTask = {
-    id: tasks.length + 1,
-    title,
-    description,
-    status: "pending",
-  };
-  tasks.push(newTask);
-  res.status(201).json(newTask).end();
-};
-
-export const updateTask = (req, res, next) => {
+export const getTaskById = async (req, res, next) => {
   const { id } = req.params;
-  const { title, description, status } = req.body;
-
-  const task = tasks.find((task) => task.id === parseInt(id));
+  const task = await prisma.task.findUnique({ where: { id: parseInt(id) } });
 
   if (!task) {
     return res.status(404).json({ message: "Task not found" }).end();
   }
 
-  task.title = title;
-  task.description = description;
-  task.status = status;
+  res.status(200).json(task).end();
+};
+
+export const createTask = async (req, res, next) => {
+  const { title, description, userId } = req.body;
+  const newTask = await prisma.task.create({
+    data: { title, description, userId },
+  });
+  res.status(201).json(newTask).end();
+};
+
+export const updateTask = async (req, res, next) => {
+  const { id } = req.params;
+  const { title, description, status } = req.body;
+
+  const task = await prisma.task.update({
+    where: { id: parseInt(id) },
+    data: { title, description, status },
+  });
 
   res.status(200).json(task).end();
 };
 
-export const deleteTask = (req, res, next) => {
+export const deleteTask = async (req, res, next) => {
   const { id } = req.params;
 
-  const taskIndex = tasks.findIndex((task) => task.id === parseInt(id));
-
-  if (taskIndex === -1) {
-    return res.status(404).json({ message: "Task not found" }).end();
-  }
-
-  tasks.splice(taskIndex, 1);
+  await prisma.task.delete({ where: { id: parseInt(id) } });
 
   res.status(204).end();
 };
