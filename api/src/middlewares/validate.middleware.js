3572b01cb86751703ffe20a7819a487d4e92b441
Jean <jean.ardaillou1@gmail.com>
2026-05-27 09:24:47 +0200

    error fixé + implementation gestion des error

--- api/src/middlewares/validate.middleware.js
+++ api/src/middlewares/validate.middleware.js
@@ -1,27 +1,33 @@
 export const validate = (schemas) => (req, res, next) => {
-  const errors = [];
+  try {
+    let errors = [];
+    throw (new Error("hello i'm a beautiful error"));
+    if (schemas.body) {
+      const result = schemas.body.safeParse(req.body);
+      if (!result.success) errors = result.error?.issues || [];
+    }
 
-  if (schemas.body) {
-    const result = schemas.body.safeParse(req.body);
-    if (!result.success) errors.push(...result.error.errors);
+    if (schemas.params) {
+      const result = schemas.params.safeParse(req.params);
+      if (!result.success) errors = result.error?.issues || [];
+    }
+
+    if (schemas.query) {
+      const result = schemas.query.safeParse(req.query);
+      if (!result.success) errors = result.error?.issues || [];
+    }
+
+    if (errors.length > 0) {
+      return res.status(400).json({
+        message: "Invalid data",
+        errors: errors.map((e) => ({ field: e.path.join("."), message: e.message })),
+      });
+    }
+    next();
+
+  } catch (error) {
+    console.log(error);
+    next(error);
   }
 
-  if (schemas.params) {
-    const result = schemas.params.safeParse(req.params);
-    if (!result.success) errors.push(...result.error.errors);
-  }
-
-  if (schemas.query) {
-    const result = schemas.query.safeParse(req.query);
-    if (!result.success) errors.push(...result.error.errors);
-  }
-
-  if (errors.length > 0) {
-    return res.status(400).json({
-      message: "Données invalides",
-      errors: errors.map((e) => ({ field: e.path.join("."), message: e.message })),
-    });
-  }
-
-  next();
 };
--- api/src/server.js
+++ api/src/server.js
@@ -1,10 +1,12 @@
 import express from "express";
 import { tasksRouter } from "./routes/tasks.routes.js";
+import { errorHandler } from "./middlewares/error.middleware.js";
 
 const app = express();
 
 app.use(express.json());
 app.use("/tasks", tasksRouter);
+app.use(errorHandler);
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
