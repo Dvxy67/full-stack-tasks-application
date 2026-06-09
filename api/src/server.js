0ef3d8583593cde0e967b1bc88c00959ea47560c
Jean <jean.ardaillou1@gmail.com>
2026-06-09 14:33:04 +0200

    un peu de css plus liste

--- api/src/server.js
+++ api/src/server.js
@@ -1,9 +1,11 @@
 import express from "express";
+import cors from "cors";
 import { tasksRouter } from "./routes/tasks.routes.js";
 import { errorHandler } from "./middlewares/error.middleware.js";
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use("/tasks", tasksRouter);
 app.use(errorHandler);
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { getTasks } from './api/tasks.api.js'
+import { useState, useEffect } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [tasks, setTasks] = useState([]);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function loadTasks() {
+      try {
+        setLoading(true);
+        const data = await getTasks();
+        setTasks(data);
+      } catch (err) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    }
+    loadTasks();
+  }, []);
+
+  if (loading) return <p className="state">Chargement...</p>;
+  if (error) return <p className="state error">Erreur : {error}</p>;
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="header">
+        <p className="header-label">Task Manager — {tasks.length} tâches</p>
+        <h1>Mes <em>tâches</em></h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <ul>
+        {tasks.map((task, index) => (
+          <li key={task.id}>
+            <span className="task-number">{String(index + 1).padStart(2, '0')}</span>
+            <div className="task-content">
+              <h2>{task.title}</h2>
+              <p>{task.description}</p>
+            </div>
+          </li>
+        ))}
+      </ul>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
